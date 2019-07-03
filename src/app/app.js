3255e3a3bf30e27ae63f0584f17671a5b698fc7f
Seonguk Choi <apoldax@gmail.com>
2019-07-03 11:14:49 +0900

    TEST commit

---
+++ src/app/app.js
@@ -0,0 +1,45 @@
+
+import {MDCTopAppBar} from '@material/top-app-bar';
+import {MDCDrawer} from "@material/drawer";
+import {MDCTextField} from '@material/textfield';
+import {MDCChipSet} from '@material/chips';
+import {MDCRipple} from '@material/ripple';
+
+
+//const ripple = new MDCRipple(document.querySelector('.foo-button'));
+
+
+
+//Instantiation
+
+const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+
+
+
+const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
+topAppBar.setScrollTarget(document.getElementById('main-content'));
+topAppBar.listen('MDCTopAppBar:nav', () => {
+  drawer.open = !drawer.open;
+});
+
+
+const textFileds = document.querySelectorAll('.mdc-text-field');
+for (const textFiled of textFileds) {
+	MDCTextField.attachTo(textFiled);
+}
+
+
+const selector = '.mdc-button, .mdc-card__primary-action, .mdc-chip, .tm-surface';
+const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
+  return new MDCRipple(el);
+});
+
+const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
+iconButtonRipple.unbounded = true;
+
+
+const chipSetEl = document.querySelector('.mdc-chip-set');
+const chipSet = new MDCChipSet(chipSetEl);
+
+
+const fabRipple = new MDCRipple(document.querySelector('.mdc-fab'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,160 @@
+'use strict';
+
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const htmlmin = require("html-minifier");
+const autoprefixer = require("autoprefixer");
+
+// 'development',production
+const dist = __dirname + '/dist'
+
+function tryResolve_(url, sourceFilename) {
+    // Put require.resolve in a try/catch to avoid node-sass failing with
+    // cryptic libsass errors
+    // when the importer throws
+    try {
+        return require.resolve(url, {
+            paths: [path.dirname(sourceFilename)]
+        });
+    } catch (e) {
+        return '';
+    }
+}
+
+function tryResolveScss(url, sourceFilename) {
+    // Support omission of .scss and leading _
+    const normalizedUrl = url.endsWith('.scss') ? url : `${url}.scss`;
+    return tryResolve_(normalizedUrl, sourceFilename) ||
+        tryResolve_(path.join(path.dirname(normalizedUrl), `_${path.basename(normalizedUrl)}`),
+            sourceFilename);
+}
+
+function materialImporter(url, prev) {
+    if (url.startsWith('@material')) {
+        const resolved = tryResolveScss(url, prev);
+        return {
+            file: resolved || url
+        };
+    }
+    return {
+        file: url
+    };
+}
+
+
 module.exports = [{
-  entry: ['./app.scss', './app.js'],
+  entry:
+	  {
+	  first:[
+          path.resolve(__dirname, 'src/public/index.html'),
+          path.resolve(__dirname, 'src/style/app.scss'),
+          path.resolve(__dirname, 'src/app/app.js')
+		  ],
+	  signin:[
+          path.resolve(__dirname, 'src/public/signin.html'),
+          path.resolve(__dirname, 'src/style/app.scss'),
+          path.resolve(__dirname, 'src/app/app.js')
+		  ]
+	  },
+	 // ['./app.scss', './app.js'],
   output: {
-    filename: 'bundle.js',
+	 filename: '[name].js',
+	 // filename: 'bundle.js',
   },
   module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'bundle.css',
-            },
-          },
-          { loader: 'extract-loader' },
-          { loader: 'css-loader' },
-          {
-            loader: 'sass-loader',
-            options: {
-              includePaths: ['./node_modules']
-            }
-          }
-        ]
-      },
-      {
+      rules: [{
+          test: /\.scss$/,
+          // test: /\.(sa|sc|c)ss$/,
+          use: [
+        	  {
+                  loader: MiniCssExtractPlugin.loader,
+              }, 
+              {
+                  loader: 'css-loader',
+              }, 
+              {
+                  loader: 'postcss-loader',
+                  options: {
+                      plugins: () => [autoprefixer()],
+                  },
+              }, 
+              {
+                  loader: 'sass-loader',
+                  options: {
+                      includePaths: [path.resolve(__dirname, 'node_modules')],
+                      importer: materialImporter
+                  },
+              }
+          ],
+      },{
         test: /\.js$/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015'],
+          presets:  ["@babel/preset-env"] ,
           plugins: ['transform-object-assign']
         },
-      }
-    ]
-  },
+      },
+      {
+          test: /\.html$/,
+          use: [{
+              loader: "html-loader",
+              options: {
+                  interpolate: true,
+                  minimize: false,
+                  removeComments: false,
+                  collapseWhitespace: true
+              }
+          }]
+      }, {
+          test: /\.svg$/,
+          use: [{
+              loader: "svg-url-loader"
+          }]
+      }, {
+          test: /\.(jpe?g|png|gif|ico)(\?.+)?$/,
+          use: {
+              loader: 'url-loader',
+              options: {
+                  limit: 8192,
+                  name: './img/[name].[ext]'
+              }
+          }
+      }],
+	},
+    plugins: [
+        new HtmlWebpackPlugin({
+        	title: 'index',
+            hash: true,
+            inject: true,
+            template: path.resolve(__dirname, 'src/public/index.html'),
+            chunks: ['first'],
+            filename: 'first.html'
+        }),
+        new HtmlWebpackPlugin({
+        	title: 'sign-in',
+            hash: true,
+            inject: true,
+            template: path.resolve(__dirname, 'src/public/signin.html'),
+            chunks: ['signin'],
+            filename: 'signin.html'
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            // filename: devMode ? '[name].css' : '[name].[hash].css',
+            // chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+            // filename: 'bundle.css',
+            chunkFilename: '[name].css'
+        })],
+        devServer: {
+            contentBase: './dist/',
+            watchContentBase: true,
+            open: true,
+            hot: true,
+            inline: true,
+            progress: true
+        }
 }];
+
