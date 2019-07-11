b4c3f15298af354967123d62c539dfca380e6d40
Seonguk Choi <apoldax@gmail.com>
2019-07-11 16:43:59 +0900

    Parallax test

--- src/app/app.js
+++ src/app/app.js
@@ -1,41 +1,68 @@
 
-import {MDCTopAppBar} from '@material/top-app-bar';
-import {MDCDrawer} from "@material/drawer";
-import {MDCTextField} from '@material/textfield';
-import {MDCChipSet} from '@material/chips';
+//import {MDCTopAppBar} from '@material/top-app-bar';
+//import {MDCDrawer} from "@material/drawer";
+//import {MDCTextField} from '@material/textfield';
+//import {MDCChipSet} from '@material/chips';
 import {MDCRipple} from '@material/ripple';
-import {MDCTextFieldIcon} from '@material/textfield/icon';
-import  * as Parallax from './../../node_modules/scroll-parallax/dist/Parallax.js';
-
+//import {MDCTextFieldIcon} from '@material/textfield/icon';
+//import  * as Parallax from './../../node_modules/scroll-parallax/dist/Parallax.js';
+import simpleParallax from './../../node_modules/simple-parallax-js'
 //const ripple = new MDCRipple(document.querySelector('.foo-button'));
 
 
 
 //Instantiation
 
-const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
 
 
 
-const topAppBar = MDCTopAppBar.attachTo(document.getElementById('tm-app-bar'));
-topAppBar.setScrollTarget(document.getElementById('tm-main-content'));
-topAppBar.listen('MDCTopAppBar:nav', () => {
-  drawer.open = !drawer.open;
-});
+//const drawerEl = document.querySelector('.mdc-drawer');
+//if (!drawerEl) {
+//	const drawer = MDCDrawer.attachTo(drawerEl);
+//}
 
 
-const textFileds = document.querySelectorAll('.mdc-text-field');
-for (const textFiled of textFileds) {
-	MDCTextField.attachTo(textFiled);
+const topAppBarEl = document.getElementById('tm-app-bar');
+
+console.log(topAppBarEl);
+if (topAppBarEl !== null) {
+	const topAppBar = MDCTopAppBar.attachTo(topAppBarEl);
+	topAppBar.listen('MDCTopAppBar:nav', () => {
+		drawer.open = !drawer.open;
+	});
+	
+	const mainContentEl = document.getElementById('tm-main-content');
+	if (mainContentEl !== null) {
+		topAppBar.setScrollTarget(document.getElementById(mainContentEl));
+	}
 }
 
 
-const selector = '.mdc-button, .mdc-card__primary-action, .tm-surface';
-const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
-  return new MDCRipple(el);
-});
 
-// .mdc-chip,
+
+const textFiledEl = document.querySelectorAll('.mdc-text-field');
+if (textFiledEl !== null) {
+	for (const textFiled of textFiledEl) {
+		MDCTextField.attachTo(textFiled);
+	}
+}
+
+
+
+const selector = '.mdc-button, .mdc-card__primary-action';
+const rippleEl = document.querySelectorAll(selector);
+if (rippleEl !== null){
+	const ripples = [].map.call(rippleEl, function(el) {
+		  return new MDCRipple(el);
+	});
+}
+
+
+
+
+
+
+// .mdc-chip, .tm-surface
 
 
 
@@ -44,22 +71,47 @@ const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
 
 
 const chipSetEl = document.querySelector('.mdc-chip-set');
-const chipSet = new MDCChipSet(chipSetEl);
+if (chipSetEl!== null){
+	const chipSet = new MDCChipSet(chipSetEl);
+}
 
 
-const fabRipple = new MDCRipple(document.querySelector('.mdc-fab'));
+const fabRippleEl = document.querySelector('.mdc-fab');
+if (fabRippleEl !== null) {
+	const fabRipple = new MDCRipple(fabRippleEl);
+}
+
 
 
 
 //const icon = new MDCTextFieldIcon(document.querySelector('.mdc-text-field-icon'));
-const icon = new MDCTextFieldIcon(document.querySelector('.mdc-text-field__icon'));
+
+
+const iconeEl = document.querySelector('.mdc-text-field__icon');
+if (iconeEl !== null) {
+	const fabRipple = new MDCTextFieldIcon(iconeEl);
+}
 
 
 
 
-var p = new Parallax('.tm-parallax', {
-	  offsetYBounds: 90,
-	  intensity: 80,
-	  center: 0.8,
-	  safeHeight: 0.15
-	}).init()
+//if (!document.querySelector('.tm-parallax')){
+//	console.log('init entry');
+//	var p = new Parallax('.tm-parallax', {
+//		offsetYBounds: 90,
+//		intensity: 10,
+//		center: 0.5,
+//		safeHeight: 0.5
+//	}).init();
+//	console.log('init');
+//}
+
+//var scene = document.getElementById('tm-parallax');
+//var parallaxInstance = new Parallax(scene, {
+ // relativeInput: true
+//});
+//parallaxInstance.friction(0.2, 0.2);
+
+
+var image = document.getElementsByClassName('tm-parallax');
+new simpleParallax(image);
